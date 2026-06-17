5989f03d584ab795715214fc23b705dbfd3f965a
Mayank2710 <makwanamayank271@gmail.com>
2026-06-17 14:22:29 +0530

    Remove backend from frontend branch

--- server/src/middlewares/notFoundMiddleware.js
+++
@@ -1,8 +0,0 @@
-const notFoundMiddleware = (req, res) => {
-  return res.status(404).json({
-    success: false,
-    message: `Route Not Found - ${req.originalUrl}`
-  });
-};
-
-export default notFoundMiddleware;
